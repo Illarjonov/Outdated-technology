0eb99f43c06328d18c7a171201bd7c18e3bbd084
illarjonov <danilgrot0@gmail.com>
2021-11-21 15:58:20 +0300

    1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React or no выф
+          Learn React or no выфdsa 
         </a>
       </header>
     </div>
